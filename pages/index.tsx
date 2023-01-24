8aa2f08774f9978a822ead76d2ebcd510a4293be
Sixten Ekblad <sixten.ekblad@gmail.com>
2023-01-24 01:00:00 +0100

    lade till localstorage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import { Button, Flex } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ProfileSelect from '../components/ProfileSelect'
 import ShoppingList from '../components/ShoppingList'
+import { supabase } from '../lib/initSupabase'
 
-export default function Kuk() {
+export default function Index() {
     const [user, setUser] = useState<any>(null)
     const [users, setUsers] = useState<[]>([])
+
+    async function getLocalUser() {
+        const localUser = localStorage.getItem('user_id')
+
+        if (localUser) {
+            let { data, error } = await supabase.from('users').select('user_name, user_id').eq('user_id', localUser)
+
+            if (error) return
+
+            if (data) setUser(data[0] as any)
+        }
+    }
+
+    useEffect(() => {
+        getLocalUser()
+    }, [])
 
     return (
         <Flex
